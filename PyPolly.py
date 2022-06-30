ae7c19037a918f1d229d08937d39bd488d6834de
Ava Mikolavich <avamikolavich@Avas-MacBook-Air.local>
2022-06-30 06:10:19 -0700

    Adding PyPolly.py file.

---
+++ PyPolly.py
@@ -0,0 +1,19 @@
+import csv
+import os
+from wsgiref import headers
+
+# Assign a variable to load a file from a path.
+file_to_load = os.path.join("resources", "election_results.csv")
+
+# Assign a variable to save the file to a path.
+file_to_save = os.path.join("analysis", "election_analysis.txt")
+
+# Open the election results and read the file.
+with open(file_to_load) as election_data:
+
+    # To do: read and analyze the data here.
+    file_reader = csv.reader(election_data)
+   
+   # Skip header row
+   headers = next(file_reader)
+   print(headers)
---
+++ python_practice.py
@@ -0,0 +1,2 @@
+file_to_save = os.path.join("analysis", "election_analysis.txt")
+open(file_to_save, "w")
